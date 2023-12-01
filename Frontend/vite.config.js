2da6dc2acf8b9e1319e21c8f7aa7de821ae2499e
Uilian <uilians320@gmail.com>
2023-12-01 17:16:53 +0100

    haciendo cambios para que funcione en lacal

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
   build: {
     rollupOptions: {
-      input: 'src/index.html'
-    }
+      input: path.resolve(__dirname, 'src', 'index.html')
+    },
+    outDir: path.resolve(__dirname, 'dist')
   },
   publicDir: 'public',
   base: './'
